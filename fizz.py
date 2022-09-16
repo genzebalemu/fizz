d9e660820445f43417c50fd4fabd46568c3ed6ee
genzebalemu <genzebalemu46@gmal.com>
2022-09-16 01:36:04 -0700

    hashing

--- fizz.py
+++ fizz.py
@@ -1,14 +1,14 @@
 
-class Solution(object):
-    def fizzBuzz(self, n):
-        list=[]
-        for i in range(n):
-                list.append(str(i+1)) 
-                if (i+1)%3 == 0 and (i+1)%5==0:
-                    list[i]="FizzBuzz"
-                elif (i+1)%3==0 and (i+1%5) !=0:
-                    list[i]="Fizz"
-                elif (i+1)%5 ==0:
-                    list[i]="Buzz"
+# class Solution(object):
+#     def fizzBuzz(self, n):
+#         list=[]
+#         for i in range(n):
+#                 list.append(str(i+1)) 
+#                 if (i+1)%3 == 0 and (i+1)%5==0:
+#                     list[i]="FizzBuzz"
+#                 elif (i+1)%3==0 and (i+1%5) !=0:
+#                     list[i]="Fizz"
+#                 elif (i+1)%5 ==0:
+#                     list[i]="Buzz"
                 
-        return list
+#         return list
