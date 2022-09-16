4918acb946a12a076047fc42b05255cb08423115
genzebalemu <genzebalemu46@gmal.com>
2022-09-16 01:03:23 -0700

    modifi the fizz.py

--- fizz.py
+++ fizz.py
@@ -1,3 +1,14 @@
-def main():
-for i in range (20):
-print("hdgshjf")
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        list=[]
+        for i in range(n):
+                list.append(str(i+1)) 
+                if (i+1)%3 == 0 and (i+1)%5==0:
+                    list[i]="FizzBuzz"
+                elif (i+1)%3==0 and (i+1%5) !=0:
+                    list[i]="Fizz"
+                elif (i+1)%5 ==0:
+                    list[i]="Buzz"
+                
+        return list
